915664ff61011f11b36e11beab8320a2444138f1
Rabenandrasana Tafinasoa <tafinasoa35@gmail.com>
2023-12-08 13:09:48 +0300

    bypass mongodb

--- src/controllers/chat.controllers.ts
+++ src/controllers/chat.controllers.ts
@@ -8,7 +8,7 @@ import { Response } from "express";
 import { ask } from "../services/langchain.services";
 import { deleteFile, fetchFile } from "../helpers/file.helper";
 import path from "path";
-import Session from "../models/session.model";
+// import Session from "../models/session.model";
 
 const tempDirectory = path.resolve(__dirname, "../tmp/");
 
@@ -39,13 +39,12 @@ export const initChatHandler = async (
       chain
     );
     const summary = await ask("Summarize the document.", chain);
-    const sessionDoc = new Session({ sessionId: req.session.id });
-    console.log();
-    await sessionDoc.save();
+    // const sessionDoc = new Session({ sessionId: req.session.id });
+    // await sessionDoc.save();
     cache[req.session.id] = chain;
     deleteFile(filename);
     return res.status(200).json({
-      sessionId: sessionDoc.id,
+      sessionId: req.session.id,
       summary: summary.text,
       questions: [...questions.text.split(/\r?\n/)],
     });
@@ -62,12 +61,12 @@ export const makeQueryHandler = async (
   res: Response
 ) => {
   try {
-    console.log("interacting", req.body.sessionId);
-    const sessionDoc = await Session.findById(req.body.sessionId);
-    if (!sessionDoc)
-      return res.status(403).json({ message: "chat not initialized" });
-    console.log("found doc : ", sessionDoc.sessionId);
-    const chain = cache[sessionDoc.sessionId];
+    // console.log("interacting", req.body.sessionId);
+    // const sessionDoc = await Session.findById(req.body.sessionId);
+    // if (!sessionDoc)
+    //   return res.status(403).json({ message: "chat not initialized" });
+    // console.log("found doc : ", sessionDoc.sessionId);
+    const chain = cache[req.body.sessionId];
     if (!chain)
       return res.status(403).json({ message: "chat not initialized" });
     const [question] = [req.body.question];
